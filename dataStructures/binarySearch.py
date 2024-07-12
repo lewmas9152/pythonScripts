cf8ae850e03eab2ea384e7ed854ee152e44bd103
lewmas9152 <smnjuguna22@gmail.com>
2024-07-12 20:48:12 +0300

    fix: ensured working for all testcases

--- dataStructures/binarySearch.py
+++ dataStructures/binarySearch.py
@@ -67,7 +67,7 @@ test9 = {
         "cards": [10,7,7,7,7,6,5,5,2],
         "query": 7
     },
-    "output": 0
+    "output": 1
 }
 
 
@@ -76,14 +76,20 @@ testCases = [test1, test2, test3, test4, test5, test6, test7, test8, test9]
 def locate_card(cards,query):
     f = 0
     l = len(cards)-1
-
-    print(f"Test:{i}")
-
     while f<=l:
         mid = (l+f)//2
         midnumber = cards[mid]
+
+        if mid >0 :
+             prevnum = cards[mid-1]
+        else:
+             prevnum=None
+        
         if midnumber == query:
-            return mid
+            if mid == 0 or prevnum != midnumber:
+                return mid
+            else:
+                 l= mid-1
         elif midnumber > query:
                 f = mid+1
         elif midnumber < query:
@@ -99,6 +105,7 @@ for i, tests in enumerate(testCases):
   
     result = locate_card(cards,query)
     print(result == output)
+    print(f"Test {i + 1}: {result == output} (Expected: {output}, Got: {result})")
 
 
 
